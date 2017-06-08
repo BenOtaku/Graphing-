8a625a53209ce79a670c138e5d8d8b5e0a0b4470
BenOtaku <camillebenoit98@yahoo.com>
2017-06-07 23:03:21 -0600

    Update Quadratic.js

--- Quadratic.js
+++ Quadratic.js
@@ -1,6 +1,6 @@
 function calculate(){
 // User's input:
- var a=prompt("enter value of a (must be numerical)"),
+     var a=prompt("enter value of a (must be numerical)"),
      b=prompt("enter value of b (must be numerical)"),
      c=prompt("enter value of c (must be numerical)"),
 // end of User's input. 
@@ -22,28 +22,26 @@ function calculate(){
      dis_sqrt=Math.sqrt(dis),
 // End of Discriminant.
 // 1st root:
-    x1=-b+dis_sqrt,
-    x1=x1/a2,
+         x1=-b+dis_sqrt,
+         x1=x1/a2,
 // End of 1st root.
 // 2nd root:
-    x2=-b-dis_sqrt,
-    x2=x2/a2,
+        x2=-b-dis_sqrt,
+        x2=x2/a2,
 // End of 2nd root.
 // Negative Discriminant:
-   negB= -1 *b,
+       negB= -1 *b,
 // end of Discriminant.
 //Positive Discriminant:
-   posDis = -1 * dis;
+       posDis = -1 * dis;
 //end of Positive Discriminant.
+
 //check the user's input:
-//if "a" is zero or null, return to the function:
      if(a == 0){
         alert("your don't have an input or it is zero so you need to input a number that is greater than zero!");
         return calculate();
         }
-// end of "a" verification.
-// if a,b or c is not a number:
-     if(isNaN(a)){
+   if(isNaN(a)){
           alert("Your input is a letter or special key, input a number therefore try again!");
           return calculate();
         }
@@ -55,17 +53,28 @@ function calculate(){
           alert("Your input is a letter or special key, input a number therefore try again!");
           return calculate();
         }
-// end of cheking a,b or c.
 // end of check user's input.
- if(dis<0){
+
+     if(dis<0){
+// if Discriminant remainder is equal to zero then simplify:
+     if(Math.sqrt(posDis) % 1 === 0){
+   var posDis = Math.sqrt(posDis);
 //Display results:
         document.getElementById('Equation').innerHTML='No real roots exist since Discriminant < 0, which mean complex numbers!<br />Discriminant = ' + dis + ' <br />The Equation = ' + a + 'x&#178; + ' + b + 'x + ' + c + '<br />';
+        document.getElementById('x1').innerHTML="Alpha (x') = " + negB + "+" + posDis + "i" + "/" + 2*a;
+        document.getElementById('x2').innerHTML="Beta (x'') = "  + negB + "-" + posDis + "i" + "/" + 2*a;
+        document.getElementById('axis').innerHTML=' Axis of symmetry = '  + ax;
+        document.getElementById('vertex').innerHTML=' Vertex = ('  + ax + ' ; ' + vx + ')';
+        }
+     else{
+       document.getElementById('Equation').innerHTML='No real roots exist since Discriminant < 0, which mean complex numbers!<br />Discriminant = ' + dis + ' <br />The Equation = ' + a + 'x&#178; + ' + b + 'x + ' + c + '<br />';
         document.getElementById('x1').innerHTML="Alpha (x') = " + negB + "+" + "&radic;"+ posDis + "<i>i</i>" + "/" + 2*a;
         document.getElementById('x2').innerHTML="Beta (x'') = "  + negB + "-" + "  &radic;"+ posDis + "<i>i</i>" + "/" + 2*a;
         document.getElementById('axis').innerHTML=' Axis of symmetry = '  + ax;
         document.getElementById('vertex').innerHTML=' Vertex = ('  + ax + ' ; ' + vx + ')';
-        }
- else if(dis===0){
+     }
+     }
+     else if(dis===0){
 //Display results:
         document.getElementById('Equation').innerHTML=" Equation = " + a + "x&#178; + " + b + "x + " + c + "<br />";
         document.getElementById('x1').innerHTML=" Alpha (x') = " + x1;
@@ -73,7 +82,7 @@ function calculate(){
         document.getElementById('vertex').innerHTML=' Vertex = ('  + ax + ' ; ' + vx + ')';
         document.getElementById('Type_root').innerHTML='The roots are repeated real number solutions because the Discriminant is zero';
      }
- else{
+     else{
 //Display results:
         document.getElementById('Equation').innerHTML=" Equation = " + a + "x&#178; + " + b + "x + " + c + "<br />";
         document.getElementById('x1').innerHTML=" Alpha (x') = " + x1;
